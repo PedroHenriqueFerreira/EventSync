ebbaa1c63b6a628dfde89e5cf71a30c63b9b8be7
PedroHenriqueFerreira <HPedro09062004@gmail.com>
2023-06-29 15:04:25 -0300

    Refactoring screen

--- views/HomeView.java
+++ views/HomeView.java
@@ -1,26 +1,15 @@
 package views;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.awt.event.AdjustmentListener;
 import java.util.ArrayList;
-import java.util.concurrent.Flow;
-
-import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
-import javax.swing.border.EmptyBorder;
-import javax.swing.plaf.basic.BasicScrollBarUI;
 
 import utils.Components;
 import utils.Constraints;
@@ -64,38 +53,61 @@ public class HomeView extends JPanel {
 
         this.add(header, gridBagConstraints);
 
-        JPanel myEventsPanel = new JPanel();
-        myEventsPanel.setBackground(getBackground());
-        
-        JScrollPane scrollPane = Components.createScrollBar(myEventsPanel);
+        JPanel mainPanel = new JPanel();
+        mainPanel.setBackground(getBackground());
 
-        gridBagConstraints.fill = GridBagConstraints.BOTH;
         gridBagConstraints.gridy = 1;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
         gridBagConstraints.weighty = 1.0;
 
-        this.add(scrollPane, gridBagConstraints);
+        this.add(mainPanel, gridBagConstraints);
+
+        JPanel myEventsPanel = new JPanel();
+        myEventsPanel.setBackground(getBackground());
+
+        JPanel allEventsPanel = new JPanel();
+        allEventsPanel.setBackground(getBackground());
+        
+        GridBagConstraints mainPanelConstraints = new GridBagConstraints();
+        mainPanelConstraints.anchor = GridBagConstraints.NORTH;
+        mainPanelConstraints.fill = GridBagConstraints.HORIZONTAL;
+        mainPanelConstraints.weightx = 1.0;
+
+        mainPanelConstraints.gridy = 0;
+        mainPanel.add(Components.createScrollBar(myEventsPanel), mainPanelConstraints);
+        mainPanelConstraints.gridy = 1;
+        mainPanel.add(Components.createScrollBar(allEventsPanel), mainPanelConstraints);
 
         for (int i = 0; i < 20; i++) {
-            JLabel name = Components.createTitle("Master Class resinas");
-    
-            JLabel description = Components.createLightText("Venha se divertir aprendendo sobre resinas wwwe wef wefwef wef wefwefwe wefwefwef");
-    
-            JLabel address = Components.createGrayText("Rua José de Alencar, 135, Cuiabá - MT");
-            JLabel date = Components.createGrayText("Quinta-feira, 29, junho");
-    
             JButton expandButton = Components.createLightButton("Ver mais detalhes");
             expandButton.addActionListener(e -> mainView.changeScreen("event"));
     
             ArrayList<JComponent> components = new ArrayList<JComponent>();
 
-            components.add(name);
-            components.add(description);
-            components.add(address);
-            components.add(date);
+            components.add(Components.createTitle("Master Class resinas"));
+            components.add(Components.createLightText("Venha se divertir aprendendo sobre resinas"));
+            components.add(Components.createGrayText("Rua José de Alencar, 135, Cuiabá - MT"));
+            components.add(Components.createGrayText("Quinta-feira, 29, junho"));
             components.add(expandButton);
     
             JPanel container = Components.createContainer(components);
             myEventsPanel.add(container);
+        }
+
+        for (int i = 0; i < 20; i++) {
+            JButton expandButton = Components.createLightButton("Ver mais detalhes");
+            expandButton.addActionListener(e -> mainView.changeScreen("event"));
+    
+            ArrayList<JComponent> components = new ArrayList<JComponent>();
+
+            components.add(Components.createTitle("Master Class resinas"));
+            components.add(Components.createLightText("Venha se divertir aprendendo sobre resinas"));
+            components.add(Components.createGrayText("Rua José de Alencar, 135, Cuiabá - MT"));
+            components.add(Components.createGrayText("Quinta-feira, 29, junho"));
+            components.add(expandButton);
+    
+            JPanel container = Components.createContainer(components);
+            allEventsPanel.add(container);
         }
 
     }
